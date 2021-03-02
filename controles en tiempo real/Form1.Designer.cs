ee1ab18fc4499d441a33ada829a340228977575a
Andrea Evelyn Mejía Rubio <andiemartin33@gmail.com>
2021-03-01 19:13:27 -0600

    Agregar archivos de proyecto.

---
+++ controles en tiempo real/Form1.Designer.cs
@@ -0,0 +1,65 @@
+﻿namespace controles_en_tiempo_real
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::controles_en_tiempo_real.Properties.Resources.trash_can;
+            this.pictureBox2.Location = new System.Drawing.Point(709, 342);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(79, 96);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            this.pictureBox2.Click += new System.EventHandler(this.pictureBox2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.WindowFrame;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.pictureBox2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox2;
+    }
+}
+
---
+++ controles en tiempo real/Form1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace controles_en_tiempo_real
+{
+    public partial class Form1 : Form
+    {
+        Button boton;
+
+        public Form1()
+        {
+            InitializeComponent();
+            int posicion = 0;
+            int indice = 0;
+
+            while (indice < 6)
+            {
+                boton = new Button();
+                //localizacion, nombre y el tamaño del boton
+                boton.Location = new Point(posicion, posicion);
+                boton.Name = "Boton a" + indice;
+                boton.Size = new Size(40, 40);
+                boton.Text = indice.ToString();
+
+                boton.Click += new EventHandler(MyEventoBoton_click);
+                this.Controls.Add(boton);
+
+                indice++;
+                posicion += 60;
+            }
+
+
+        }
+
+
+        private void MyEventoBoton_click(object sender, EventArgs e)
+        {
+
+            Button myboton = sender as Button;
+            MessageBox.Show("Soy el boton" + myboton.Name);
+            myboton.Text = "Click" +myboton.Text;
+
+        }
+
+        
+
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+    
+}
